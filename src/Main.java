8af188ca16ce9ca6ccc348da31e4858a6a233a47
888Andrey888 <andrey.reshetoff.88@gmail.com>
2023-04-04 19:45:25 +0600

    finished addItemInArray

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ public class Main {
     }
 
     // Метод для заполнения массива случайными числами
-    public static double[] addItemInArray(double array[]) {
-        return array;
+    public static double[] addItemInArray(int arrayLength, int min, int max) {
+        double arrayToReturn[] = new double[arrayLength];
+        for (int i = 0; i < arrayLength; i ++) {
+            arrayToReturn[i] = generateRandomItem(min, max);
+        }
+        return arrayToReturn;
     }
 
     // Метод генерирующий случайные дробные числа
