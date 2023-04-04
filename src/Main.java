1bf822d4933f26771a02b1968ac77c7ce516e78b
888Andrey888 <andrey.reshetoff.88@gmail.com>
2023-04-04 21:37:42 +0600

    finished average

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         double array[] = new double[15];
+        //Написал метод заполнения массива случайными положительными и отрицательными числами было лень придумывать самому (программисты ленивые люди)
         array = addItemInArray(array.length, -10, 10);
         for(double i: array){
             System.out.println(i);
         }
 
-        int previousValue = 0;
-        for(double i: array){
-            if(previousValue < 0){
+        System.out.println();
 
+        double previousValues = 0;
+        boolean startAddition = false;
+        int countOfIntNum = 0;
+        double summ = 0;
+        for(double i: array){
+            if(i > 0 && previousValues < 0)
+                startAddition = true;
+
+            if(startAddition && i > 0){
+                summ += i;
+                countOfIntNum++;
             }
+
+            previousValues = i;
         }
+
+        System.out.println(summ / countOfIntNum);
     }
 
     // Метод для заполнения массива случайными числами
     public static double[] addItemInArray(int arrayLength, int min, int max) {
         double arrayToReturn[] = new double[arrayLength];
         for (int i = 0; i < arrayLength; i ++) {
+            //так же добавил метод генерации случайных дробных чисел в указанном диапазоне с расчетом что еще где пригодится(не повторяй сам себя)
             arrayToReturn[i] = generateRandomItem(min, max);
         }
         return arrayToReturn;
