286233d9b72e173478673876fae5a3176825bbcf
888Andrey888 <andrey.reshetoff.88@gmail.com>
2023-04-04 19:24:20 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        double array[] = new double[15];
+    }
+
+    // Метод для заполнения массива случайными числами
+    public static double[] addItemInArray(double array[]){
+
+    }
+
+    // Метод генерирующий случайные дробные числа
+    public static double generateRandomItem(int min, int max){
+        
+    }
+}
